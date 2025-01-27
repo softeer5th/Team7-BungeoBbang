1e031ff1809b9114f6345b7cf46d66e57f788e92
zoouniak <jueun1025@naver.com>
2025-01-27 13:51:30 +0900

    refactor : 사용하지 않는 Import문 제거

--- backend/src/main/java/com/bungeobbang/backend/agenda/domain/Agenda.java
+++ backend/src/main/java/com/bungeobbang/backend/agenda/domain/Agenda.java
@@ -10,7 +10,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Set;
 
 import static jakarta.persistence.EnumType.STRING;
