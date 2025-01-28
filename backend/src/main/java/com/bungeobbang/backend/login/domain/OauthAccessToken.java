ab72e88deaa05719ef8ef3ae94605ba557ed2376
scc9811 <wjsrudtjr98@naver.com>
2025-01-28 14:56:32 +0900

    feat : #32 OauthAccessToken 생성
    * kakao oauth 서버에서 응답받은 access/refresh token 저장하기 위함.

---
+++ backend/src/main/java/com/bungeobbang/backend/login/domain/OauthAccessToken.java
@@ -0,0 +1,25 @@
+package com.bungeobbang.backend.login.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Getter
+@NoArgsConstructor(access = PRIVATE)
+public class OauthAccessToken {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("expires_in")
+    private int expiresIn;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+    @JsonProperty("refresh_token_expires_in")
+    private int refreshTokenExpiresIn;
+
+}
