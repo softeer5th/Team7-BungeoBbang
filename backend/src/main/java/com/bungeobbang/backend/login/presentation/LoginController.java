fc3316181ddedde6a4c8e0daefe24d1ec9eac0a2
scc9811 <wjsrudtjr98@naver.com>
2025-01-29 01:23:49 +0900

    feat : #32 카카오 소셜 로그인 시 응답

---
+++ backend/src/main/java/com/bungeobbang/backend/login/presentation/LoginController.java
@@ -0,0 +1,25 @@
+package com.bungeobbang.backend.login.presentation;
+
+import com.bungeobbang.backend.login.dto.response.LoginResponse;
+import com.bungeobbang.backend.login.service.LoginService;
+import com.bungeobbang.backend.member.domain.ProviderType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class LoginController {
+
+    private final LoginService loginService;
+
+    @GetMapping("/student/auth/{provider}/login")
+    public ResponseEntity<LoginResponse> login(
+            @PathVariable final String provider,
+            @RequestParam final String code
+            ) {
+        ProviderType providerType = ProviderType.fromString(provider);
+        LoginResponse loginResponse = loginService.login(providerType, code);
+        return ResponseEntity.ok(loginResponse);
+    }
+}
---
+++ backend/src/main/java/com/bungeobbang/backend/login/service/LoginService.java
@@ -0,0 +1,60 @@
+package com.bungeobbang.backend.login.service;
+
+import com.bungeobbang.backend.common.exception.AuthException;
+import com.bungeobbang.backend.login.domain.*;
+import com.bungeobbang.backend.login.dto.response.LoginResponse;
+import com.bungeobbang.backend.login.infrastructure.JwtProvider;
+import com.bungeobbang.backend.member.domain.Member;
+import com.bungeobbang.backend.member.domain.ProviderType;
+import com.bungeobbang.backend.member.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import static com.bungeobbang.backend.common.exception.ErrorCode.MEMBER_CREATION_FAILED;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final OauthProviders oauthProviders;
+    private final MemberRepository memberRepository;
+    private final JwtProvider jwtProvider;
+    private final JedisPool jedisPool;
+
+    public LoginResponse login(final ProviderType providerType, final String code) {
+        final OauthProvider oauthProvider = oauthProviders.mapping(providerType);
+        final OauthUserInfo oauthUserInfo = oauthProvider.getUserInfo(code);
+        final Member member = findOrCreateMember(oauthUserInfo.getSocialLoginId(), providerType);
+        return getLoginResultResponse(member);
+    }
+
+    private LoginResponse getLoginResultResponse(final Member member) {
+        if (member.getUniversity() == null) {
+            return new LoginResponse(member.getId(), false, "", "");
+        }
+        final MemberTokens memberTokens = jwtProvider.generateLoginToken(member.getId().toString());
+        saveRefreshToken(member, memberTokens.getRefreshToken());
+
+        return new LoginResponse(member.getId(), true, memberTokens.getAccessToken(), memberTokens.getRefreshToken());
+    }
+
+    private void saveRefreshToken(final Member member, final String refreshToken) {
+        Jedis jedis = jedisPool.getResource();
+        jedis.setex(String.format("refreshToken%s", member.getId()), 604800, refreshToken);
+    }
+
+    private Member findOrCreateMember(final String socialLoginId, ProviderType providerType) {
+        return memberRepository.findByLoginId(socialLoginId)
+                .orElseGet(() -> createMember(socialLoginId, providerType));
+    }
+
+    private Member createMember(final String socialLoginId, ProviderType providerType) {
+        if (memberRepository.existsByLoginId(socialLoginId)) {
+            throw new AuthException(MEMBER_CREATION_FAILED);
+        }
+        Member member = new Member(socialLoginId, providerType);
+        return memberRepository.save(member);
+    }
+}
