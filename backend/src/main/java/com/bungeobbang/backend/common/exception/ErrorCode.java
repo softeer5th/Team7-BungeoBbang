174de243c7533d67dc1ee7c9655774098f83b86b
scc9811 <wjsrudtjr98@naver.com>
2025-01-29 01:30:00 +0900

    feat : #32 에러코드 추가

--- backend/src/main/java/com/bungeobbang/backend/common/exception/ErrorCode.java
+++ backend/src/main/java/com/bungeobbang/backend/common/exception/ErrorCode.java
@@ -9,7 +9,8 @@ public enum ErrorCode {
 
     // Auth
     INVALID_AUTHORIZATION_CODE(HttpStatus.BAD_REQUEST, "유효하지 않은 인증 코드입니다."),
-    NOT_SUPPORTED_OAUTH_SERVICE(HttpStatus.NOT_IMPLEMENTED, "해당 OAuth 서비스는 제공하지 않습니다.");
+    NOT_SUPPORTED_OAUTH_SERVICE(HttpStatus.NOT_IMPLEMENTED, "해당 OAuth 서비스는 제공하지 않습니다."),
+    MEMBER_CREATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "회원 생성 중 오류가 발생했습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
