561a782ee7dacd37c8c5d37b04522d1aa1418d69
scc9811 <wjsrudtjr98@naver.com>
2025-01-28 14:53:28 +0900

    feat : #32 KakaoOauthProvider 생성
    * 구글 등의 소셜로그인 추가를 위해 OauthProvider, OauthProviders 구현

---
+++ backend/src/main/java/com/bungeobbang/backend/login/domain/OauthProvider.java
@@ -0,0 +1,11 @@
+package com.bungeobbang.backend.login.domain;
+
+import org.springframework.web.client.RestTemplate;
+
+public interface OauthProvider {
+
+    RestTemplate restTemplate = new RestTemplate();
+
+    boolean is(String name);
+    OauthUserInfo getUserInfo(String code);
+}
