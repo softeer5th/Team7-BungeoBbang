c40a2392c4bb18db41931cf17a2eef576fdf74db
scc9811 <wjsrudtjr98@naver.com>
2025-01-29 10:55:49 +0900

    refactor : #32 MemberTokens Record로 변경

--- backend/src/main/java/com/bungeobbang/backend/member/domain/MemberTokens.java
+++ backend/src/main/java/com/bungeobbang/backend/member/domain/MemberTokens.java
@@ -1,12 +1,4 @@
 package com.bungeobbang.backend.member.domain;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-@Getter
-public class MemberTokens {
-
-    private final String refreshToken;
-    private final String accessToken;
+public record MemberTokens(String accessToken, String refreshToken) {
 }
