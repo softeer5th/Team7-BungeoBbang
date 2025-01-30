f108f38c89ba89036b0d8eac151002442828a415
zoouniak <jueun1025@naver.com>
2025-01-30 19:08:18 +0900

    fix: #47 소셜 로그인 Post로 변경

--- backend/src/main/java/com/bungeobbang/backend/member/presentation/MemberController.java
+++ backend/src/main/java/com/bungeobbang/backend/member/presentation/MemberController.java
@@ -1,14 +1,15 @@
 package com.bungeobbang.backend.member.presentation;
 
 import com.bungeobbang.backend.member.domain.ProviderType;
+import com.bungeobbang.backend.member.dto.request.SocialLoginRequest;
 import com.bungeobbang.backend.member.dto.response.MemberLoginResponse;
 import com.bungeobbang.backend.member.dto.response.MemberLoginResult;
 import com.bungeobbang.backend.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,13 +18,13 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @GetMapping("/student/auth/{provider}/login")
+    @PostMapping("/student/auth/{provider}/login")
     public ResponseEntity<MemberLoginResponse> login(
             @PathVariable final String provider,
-            @RequestParam final String code
-            ) {
+            @RequestBody final SocialLoginRequest request
+    ) {
         final ProviderType providerType = ProviderType.fromString(provider);
-        final MemberLoginResult memberLoginResult = memberService.login(providerType, code);
+        final MemberLoginResult memberLoginResult = memberService.login(providerType, request.code());
         return ResponseEntity.ok()
                 .header("Authorization", String.format("Bearer %s", memberLoginResult.accessToken()))
                 .body(new MemberLoginResponse(memberLoginResult.memberId(), memberLoginResult.refreshToken()));
