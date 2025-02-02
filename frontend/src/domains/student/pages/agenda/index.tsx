6107a77ca1b930aa5b1a96679b8f6a6f7b25174d
uuranus <hyur0920@gmail.com>
2025-02-02 13:55:10 +0900

    feat: 학생 답해요 진입 페이지 UI

--- frontend/src/domains/student/pages/agenda/index.tsx
+++ frontend/src/domains/student/pages/agenda/index.tsx
@@ -5,69 +5,77 @@ import BannerImg from '@/assets/imgs/school_banner.png';
 import { TopAppBar } from '@/components/TopAppBar';
 import { BottomNavigationItemProps } from '@/components/bottom-navigation/BottomNavigationItem';
 import { useTheme } from 'styled-components';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { ChatListCardData } from './ChatListCardData';
+import { ChatRoomListItem } from './chat-list-item/ChatListItem.tsx';
 
 const AgendaPage = () => {
   const theme = useTheme();
 
-  const [chatRooms, setChatRooms] = useState([
+  const [chatRooms, setChatRooms] = useState<ChatListCardData[]>([]);
+
+  const mockData = [
     {
       roomId: '1',
       dday: 'D-2',
       iconSrc: '/src/assets/icons/school.svg',
       iconBackgroundColor: theme?.colors.icnGreen,
-      title: '2025학년도 1학기 수강 신청 수오 조사',
+      title: '2025학년도 1학기 수강 신청 수요 조사',
       numOfJoin: 0,
-      isInProgress: false,
+      isInProgress: true,
     },
     {
       roomId: '2',
       dday: 'D-7',
       iconSrc: '/src/assets/icons/school.svg',
       iconBackgroundColor: theme.colors.icnGreen,
-      title: '2025학년도 1학기 수강 신청 수오 조사',
+      title: '2025학년도 1학기 수강 신청 수요 조사',
       numOfJoin: 0,
-      isInProgress: false,
+      isInProgress: true,
     },
     {
       roomId: '3',
       dday: 'D+2',
       iconSrc: '/src/assets/icons/school.svg',
       iconBackgroundColor: theme.colors.icnGreen,
-      title: '2025학년도 1학기 수강 신청 수오 조사',
+      title: '2025학년도 1학기 수강 신청 수요 조사',
       numOfJoin: 2,
-      isInProgress: true,
+      isInProgress: false,
     },
     {
       roomId: '4',
       dday: 'D+2',
       iconSrc: '/src/assets/icons/school.svg',
       iconBackgroundColor: theme.colors.icnGreen,
-      title: '2025학년도 1학기 수강 신청 수오 조사',
-      numOfJoin: 2,
-      isInProgress: true,
+      title: '2025학년도 1학기 수강 신청 수요 조사',
+      numOfJoin: 5,
+      isInProgress: false,
     },
     {
       roomId: '5',
       dday: 'D+2',
       iconSrc: '/src/assets/icons/school.svg',
       iconBackgroundColor: theme.colors.icnGreen,
-      title: '2025학년도 1학기 수강 신청 수오 조사',
+      title: '2025학년도 1학기 수강 신청 수요 조사',
       numOfJoin: 21,
-      isInProgress: true,
+      isInProgress: false,
     },
-  ]);
+  ];
+
+  useEffect(() => {
+    setChatRooms(mockData);
+  }, []);
 
   const bottomItems: BottomNavigationItemProps[] = [
     {
-      itemId: 'option',
+      itemId: 'agenda',
       iconSrc: '/src/assets/icons/message.svg',
-      title: '말해요',
+      title: '답해요',
     },
     {
-      itemId: 'agenda',
+      itemId: 'opinion',
       iconSrc: '/src/assets/icons/home.svg',
-      title: '답해요',
+      title: '말해요',
     },
     {
       itemId: 'my',
@@ -85,43 +93,25 @@ const AgendaPage = () => {
         backgroundColor={theme.colors.grayScale10}
         onRightIconClick={() => {}}
       />
-      <S.BannerContainer>
-        <S.TextContainer>
-          <S.TitleText variant="heading1">함께 만들어 나가요</S.TitleText>
-          <S.SubText variant="body3">학생회의 답변을 확인할 수 있어요.</S.SubText>
-        </S.TextContainer>
-        <S.BannerImage src={BannerImg} />
-      </S.BannerContainer>
+      <S.ContentContainer>
+        <S.BannerContainer>
+          <S.TextContainer>
+            <S.TitleText variant="heading1">함께 만들어 나가요</S.TitleText>
+            <S.SubText variant="body3">학생회의 답변을 확인할 수 있어요.</S.SubText>
+          </S.TextContainer>
+          <S.BannerImage src={BannerImg} />
+        </S.BannerContainer>
 
-      <S.ChatRoomList>
-        {chatRooms && chatRooms.length > 0 ? (
-          chatRooms.map((room) => (
-            <S.ChatRoomListItem key={room.roomId}>
-              <S.DDayTextContainer>
-                <S.DDayText variant="heading2">{room.dday}</S.DDayText>
-              </S.DDayTextContainer>
-              <S.ContentWrapper>
-                <S.IconContainer>
-                  <S.IconBox backgroundColor={room.iconBackgroundColor}>
-                    <img src={room.iconSrc} width="17px" height="17px" />
-                  </S.IconBox>
-                </S.IconContainer>
-                <S.CardTitleText variant="heading2">{room.title}</S.CardTitleText>
-              </S.ContentWrapper>
-              <S.JoinContainer>
-                <S.JoinNumberText variant="caption1">{room.numOfJoin}명</S.JoinNumberText>
-                <S.ProgressText variant="caption1">
-                  {room.isInProgress ? '참여중' : '참여 완료'}
-                </S.ProgressText>
-              </S.JoinContainer>
-            </S.ChatRoomListItem>
-          ))
-        ) : (
-          <S.EmptyTextWrapper>
-            <S.EmptyText variant="heading4">현재 개설된 채팅방이 없습니다.</S.EmptyText>
-          </S.EmptyTextWrapper>
-        )}
-      </S.ChatRoomList>
+        <S.ChatRoomList>
+          {chatRooms && chatRooms.length > 0 ? (
+            chatRooms.map((room) => <ChatRoomListItem room={room} onCardClick={() => {}} />)
+          ) : (
+            <S.EmptyTextWrapper>
+              <S.EmptyText variant="heading4">현재 개설된 채팅방이 없습니다.</S.EmptyText>
+            </S.EmptyTextWrapper>
+          )}
+        </S.ChatRoomList>
+      </S.ContentContainer>
 
       <BottomNavigation startDestination="option" destinations={bottomItems} />
     </S.Container>
--- frontend/src/domains/student/pages/agenda/styles.ts
+++ frontend/src/domains/student/pages/agenda/styles.ts
@@ -10,12 +10,19 @@ export const Container = styled.div`
   overflow: hidden;
 `;
 
+export const ContentContainer = styled.div`
+  flex: 1;
+  display: column;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.colors.grayScale10};
+`;
+
 export const BannerContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: flex-start;
   padding: 32px 27px 0px 27px;
-  background-color: ${(props) => props.theme.colors.grayScale10};
   gap: 30px;
 `;
 
@@ -39,22 +46,15 @@ export const ChatRoomList = styled.div`
   flex: 1;
   position: relative;
   top: -33px;
-  padding: 17px 16px 17px 16px;
+  padding: 17px 16px 0px 16px;
   display: grid;
   grid-template-columns: 1fr 1fr;
-   grid-auto-rows: min-content; 
+  grid-auto-rows: min-content;
   gap: 15px;
-  flex-grow:1;
+  flex-grow: 1;
 `;
 
-export const ChatRoomListItem = styled.div`
-  background-color: ${(props) => props.theme.colors.grayScaleWhite};
-  border-radius: 16px;
-  padding: 12px;
-  aspect-ratio: 1/1;
-  display: flex;
-  flex-direction: column;
-`;
+
 
 export const EmptyTextWrapper = styled.div`
   width: 100%;
@@ -66,64 +66,4 @@ export const EmptyTextWrapper = styled.div`
 
 export const EmptyText = styled(Typography)`
   color: ${(props) => props.theme.colors.grayScale40};
-`;
-
-export const DDayTextContainer = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-`;
-
-export const DDayText = styled(Typography)`
-  color: ${(props) => props.theme.colors.sementicMain};
-`;
-
-export const ContentWrapper = styled.div`
-  flex:1;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  align-items: flex-start;
-`;
-
-export const IconContainer = styled.div`
-  width : 100%;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-`;
-
-export const IconBox = styled.div<{
-  backgroundColor: string
-}>`
-  width: 30px;
-  height: 30px;
-  background-color: ${(props) => props.backgroundColor};
-  border-radius: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const CardTitleText = styled(Typography)`
-  color: ${(props) => props.theme.colors.grayScale100};
-  margin-top: 6px;
-`;
-
-export const JoinContainer = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  margin-top: 6px;
-`;
-
-export const JoinNumberText = styled(Typography)`
- color: ${(props) => props.theme.colors.sementicMain};
-`;
-
-export const ProgressText = styled(Typography)`
-  color: ${(props) => props.theme.colors.grayScale50};
-  margin-left: 4px;
 `;
