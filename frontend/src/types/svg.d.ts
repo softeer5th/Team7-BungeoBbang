83d22d1744497a40ad915e93d57bae43c59dcccf
elect10 <wlgnsakswp@naver.com>
2025-02-03 10:36:03 +0900

    fix: build error

--- frontend/src/types/svg.d.ts
+++ frontend/src/types/svg.d.ts
@@ -6,7 +6,6 @@ declare module '*.svg' {
 }
 
 declare module '*.svg?react' {
-  import * as React from 'react';
   const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
   export default ReactComponent;
 }
