a4910db6849e52d36e608ff77347c56ef35e11d5
scc9811 <wjsrudtjr98@naver.com>
2025-02-03 17:04:22 +0900

    feat : #70 MemberOpinionInfoResponse 변경 * 빌더 추가

--- backend/src/main/java/com/bungeobbang/backend/opinion/dto/response/MemberOpinionInfoResponse.java
+++ backend/src/main/java/com/bungeobbang/backend/opinion/dto/response/MemberOpinionInfoResponse.java
@@ -2,10 +2,11 @@ package com.bungeobbang.backend.opinion.dto.response;
 
 import com.bungeobbang.backend.common.type.CategoryType;
 import com.bungeobbang.backend.opinion.domain.OpinionType;
+import lombok.Builder;
 
 import java.time.LocalDateTime;
 
-public record MemberOpinionInfo(
+@Builder
 public record MemberOpinionInfoResponse(
         Long opinionId,
         OpinionType opinionType,
