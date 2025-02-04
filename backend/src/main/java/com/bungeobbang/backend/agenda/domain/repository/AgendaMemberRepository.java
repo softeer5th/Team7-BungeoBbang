952a36feee9c5086f8d87b34bc06ef07905a60ae
zoouniak <jueun1025@naver.com>
2025-02-04 16:04:48 +0900

    fix : n+1 문제 해결

--- backend/src/main/java/com/bungeobbang/backend/agenda/domain/repository/AgendaMemberRepository.java
+++ backend/src/main/java/com/bungeobbang/backend/agenda/domain/repository/AgendaMemberRepository.java
@@ -3,6 +3,8 @@ package com.bungeobbang.backend.agenda.domain.repository;
 import com.bungeobbang.backend.agenda.domain.AgendaMember;
 import com.bungeobbang.backend.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +12,10 @@ import java.util.List;
 @Repository
 public interface AgendaMemberRepository extends JpaRepository<AgendaMember, Long> {
     boolean existsByMemberIdAndAgendaId(Long memberId, Long agendaId);
+
     void deleteByMemberIdAndAgendaId(Long memberId, Long agendaId);
 
-    List<AgendaMember> findAllByMember(Member member);
+    @Query("SELECT am FROM AgendaMember am JOIN FETCH am.agenda WHERE am.member = :member")
+    List<AgendaMember> findAllByMember(@Param("member") Member member);
+
 }
