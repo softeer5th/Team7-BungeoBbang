f20d3dd89957c4278bba93b4bd504fd29ef24eec
scc9811 <wjsrudtjr98@naver.com>
2025-02-04 18:09:53 +0900

    chore : #70 누락된 import 추가

--- backend/src/main/java/com/bungeobbang/backend/opinion/presentation/OpinionChatController.java
+++ backend/src/main/java/com/bungeobbang/backend/opinion/presentation/OpinionChatController.java
@@ -7,13 +7,13 @@ import com.bungeobbang.backend.opinion.presentation.api.OpinionChatApi;
 import com.bungeobbang.backend.opinion.service.OpinionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
