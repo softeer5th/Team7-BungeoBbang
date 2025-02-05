01a0e9a9056298e17b9951021717cbef84bd4fad
zoouniak <jueun1025@naver.com>
2025-02-05 10:29:41 +0900

    test: 테스트 fixture 변경

--- backend/src/test/java/com/bungeobbang/backend/admin/fixture/AdminFixture.java
+++ backend/src/test/java/com/bungeobbang/backend/admin/fixture/AdminFixture.java
@@ -2,14 +2,23 @@ package com.bungeobbang.backend.admin.fixture;
 
 import com.bungeobbang.backend.admin.domain.Admin;
 
-import static com.bungeobbang.backend.university.UniversityFixture.UNIVERSITY;
+import static com.bungeobbang.backend.university.UniversityFixture.KAKAO_UNIVERSITY;
+import static com.bungeobbang.backend.university.UniversityFixture.NAVER_UNIVERSITY;
 
 public class AdminFixture {
-    public static Admin ADMIN = new Admin(
+    public static Admin NAVER_ADMIN = new Admin(
             1L,
             "loginId",
             "password",
-            "학생회",
-            UNIVERSITY
+            "네이버 학생회",
+            NAVER_UNIVERSITY
+    );
+
+    public static Admin KAKAO_ADMIN = new Admin(
+            1L,
+            "loginId",
+            "password",
+            "카카오 학생회",
+            KAKAO_UNIVERSITY
     );
 }
--- backend/src/test/java/com/bungeobbang/backend/university/UniversityFixture.java
+++ backend/src/test/java/com/bungeobbang/backend/university/UniversityFixture.java
@@ -3,9 +3,15 @@ package com.bungeobbang.backend.university;
 import com.bungeobbang.backend.university.domain.University;
 
 public class UniversityFixture {
-    public static University UNIVERSITY = new University(
+    public static University NAVER_UNIVERSITY = new University(
             1L,
             "네이버대학교",
             "naver.com");
+
+    public static University KAKAO_UNIVERSITY = new University(
+            2L,
+            "카카오대학교",
+            "kakao.com"
+    );
 
 }
