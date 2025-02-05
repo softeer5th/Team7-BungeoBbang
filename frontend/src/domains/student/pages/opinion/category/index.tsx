430457d70f9aa70f9c4ff5e286aaa04ab8fea1a7
elect10 <wlgnsakswp@naver.com>
2025-02-05 17:09:44 +0900

    chore: linting

--- frontend/src/domains/student/pages/opinion/category/index.tsx
+++ frontend/src/domains/student/pages/opinion/category/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { TopAppBar } from '@/components/TopAppBar';
 import { ChatSendField } from '@/components/Chat/ChatSendField';
