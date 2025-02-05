e4f6be8c6586cc044258ae3ee2de6f158d948f4e
zoouniak <jueun1025@naver.com>
2025-02-05 17:44:30 +0900

    fix : @EnableAsync로 수정

--- backend/src/main/java/com/bungeobbang/backend/common/config/AsyncConfig.java
+++ backend/src/main/java/com/bungeobbang/backend/common/config/AsyncConfig.java
@@ -1,9 +1,9 @@
 package com.bungeobbang.backend.common.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
-@EnableJpaAuditing
+@EnableAsync
 public class AsyncConfig {
 }
