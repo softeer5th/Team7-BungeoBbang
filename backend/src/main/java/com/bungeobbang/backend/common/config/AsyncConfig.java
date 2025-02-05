14a362613c63f75c4310f5cd52e9fc70f5e6b67c
zoouniak <jueun1025@naver.com>
2025-02-05 17:10:45 +0900

    fix: config 분리

---
+++ backend/src/main/java/com/bungeobbang/backend/common/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.bungeobbang.backend.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AsyncConfig {
+}
