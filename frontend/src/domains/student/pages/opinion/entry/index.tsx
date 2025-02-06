d23ab3e5681d4856f1b39e62697fc5df342d91e8
elect10 <wlgnsakswp@naver.com>
2025-02-06 12:19:48 +0900

    feat: api connect in entry point and hide stat when opinion less then 3

--- frontend/src/domains/student/pages/opinion/entry/index.tsx
+++ frontend/src/domains/student/pages/opinion/entry/index.tsx
@@ -6,6 +6,8 @@ import SchoolIcon from '@/assets/imgs/img_school.svg?react';
 import PersonIcon from '@/assets/imgs/img_person.svg?react';
 import * as S from './styles';
 import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import api from '@/utils/api';
 
 const OpinionEntryPage = () => {
   const destinations = [
@@ -26,11 +28,26 @@ const OpinionEntryPage = () => {
       hasAlarm: true,
     },
   ];
-
   const navigate = useNavigate();
 
+  const [statistic, setStatistic] = useState({ opinionCount: 0, adminResponseRate: 0 });
+  const [showStatistic, setShowStatistic] = useState(true);
+
+  useEffect(() => {
+    const getStatistic = async () => {
+      try {
+        const response = await api.get('/student/opinions');
+        console.log(response.data);
+        response.data.opinionCount < 4 ? setShowStatistic(false) : setStatistic(response.data);
+      } catch (error) {
+        setShowStatistic(false);
+        console.log(error);
+      }
+    };
+    getStatistic();
+  }, []);
   return (
-    <S.OpinionEntryContainer>
+    <>
       <TopAppBar
         leftIconSrc="/src/assets/icons/logo.svg"
         rightIconSrc="/src/assets/icons/logout.svg"
@@ -38,53 +55,57 @@ const OpinionEntryPage = () => {
         foregroundColor="#ffffff"
       />
 
-      <S.TitleInputContainer>
-        <S.TitleWrapper>
-          <Typography variant="display2">더 나은 학교를 위해</Typography>
-          <Typography variant="display2">여러분의 생각을 들려주세요!</Typography>
-        </S.TitleWrapper>
-        <S.SubTitle>여러분의 의견에 대해 빠른 시일에 답변드리겠습니다.</S.SubTitle>
-        <S.InputContainer
-          onClick={() => {
-            navigate('/opinion/category');
-          }}
-        >
-          <S.Input />
-          <S.SendButton>
-            <ArrowUp fill="#ffffff" stroke="#ffffff" />
-          </S.SendButton>
-        </S.InputContainer>
-      </S.TitleInputContainer>
-      <S.StatisticWrapper>
-        <S.StatisticContainer>
-          <S.StatLabel>
-            최근 30일
-            <br />
-            학생의 의견 수
-          </S.StatLabel>
-          <S.StatValue>
-            15건
-            <S.StatIcon>
-              <SchoolIcon />
-            </S.StatIcon>
-          </S.StatValue>
-        </S.StatisticContainer>
-        <S.StatisticContainer>
-          <S.StatLabel>
-            최근 30일
-            <br />
-            학생회의 답변율
-          </S.StatLabel>
-          <S.StatValue>
-            86%
-            <S.StatIcon>
-              <PersonIcon />
-            </S.StatIcon>
-          </S.StatValue>
-        </S.StatisticContainer>
-      </S.StatisticWrapper>
+      <S.OpinionEntryContainer>
+        <S.TitleInputContainer>
+          <S.TitleWrapper>
+            <Typography variant="display2">더 나은 학교를 위해</Typography>
+            <Typography variant="display2">여러분의 생각을 들려주세요!</Typography>
+          </S.TitleWrapper>
+          <S.SubTitle>여러분의 의견에 대해 빠른 시일에 답변드리겠습니다.</S.SubTitle>
+          <S.InputContainer
+            onClick={() => {
+              navigate('/opinion/category');
+            }}
+          >
+            <S.Input />
+            <S.SendButton>
+              <ArrowUp fill="#ffffff" stroke="#ffffff" />
+            </S.SendButton>
+          </S.InputContainer>
+        </S.TitleInputContainer>
+        {showStatistic && (
+          <S.StatisticWrapper>
+            <S.StatisticContainer>
+              <S.StatLabel>
+                최근 30일
+                <br />
+                학생의 의견 수
+              </S.StatLabel>
+              <S.StatValue>
+                {statistic.opinionCount}건
+                <S.StatIcon>
+                  <SchoolIcon />
+                </S.StatIcon>
+              </S.StatValue>
+            </S.StatisticContainer>
+            <S.StatisticContainer>
+              <S.StatLabel>
+                최근 30일
+                <br />
+                학생회의 답변율
+              </S.StatLabel>
+              <S.StatValue>
+                {statistic.adminResponseRate}%
+                <S.StatIcon>
+                  <PersonIcon />
+                </S.StatIcon>
+              </S.StatValue>
+            </S.StatisticContainer>
+          </S.StatisticWrapper>
+        )}
+      </S.OpinionEntryContainer>
       <BottomNavigation startDestination="home" destinations={destinations} />
-    </S.OpinionEntryContainer>
+    </>
   );
 };
 
