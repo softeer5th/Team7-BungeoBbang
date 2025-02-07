94fd5a616a0d37a1dc4abebe51f03bfd3876dfaf
zoouniak <jueun1025@naver.com>
2025-02-07 17:11:15 +0900

    feat: ObjectMapper에 timeModule 추가

--- backend/src/main/java/com/bungeobbang/backend/common/config/ObjectMapperConfig.java
+++ backend/src/main/java/com/bungeobbang/backend/common/config/ObjectMapperConfig.java
@@ -1,6 +1,8 @@
 package com.bungeobbang.backend.common.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +10,8 @@ import org.springframework.context.annotation.Configuration;
 public class ObjectMapperConfig {
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        return new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
 }
