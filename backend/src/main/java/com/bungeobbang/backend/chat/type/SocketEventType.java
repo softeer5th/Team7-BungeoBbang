7081b55513d6c712d1cdfbe113f8fd879a203b2d
zoouniak <jueun1025@naver.com>
2025-02-08 10:45:14 +0900

    feat: #121 웹소켓 이벤트 추가

--- backend/src/main/java/com/bungeobbang/backend/chat/type/SocketEventType.java
+++ backend/src/main/java/com/bungeobbang/backend/chat/type/SocketEventType.java
@@ -6,6 +6,9 @@ public enum SocketEventType {
     LEAVE("채팅방 화면 이탈"),
     EXIT("채팅방 나가기"),
     PARTICIPATE("채팅방 참여하기"),
+    START("채팅방 시작"),
+    CLOSE("채팅방 종료"),
+    DELETE("채팅방 삭제"),
     ERROR("에러");
 
     SocketEventType(String description) {
