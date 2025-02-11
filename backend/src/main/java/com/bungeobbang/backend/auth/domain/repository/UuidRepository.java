4cabd5234c01dbd45581d0e20faa454080cebb24
scc9811 <wjsrudtjr98@naver.com>
2025-02-11 15:01:32 +0900

    feat : #186 uuid 저장 기간 추가
    * 어세스토큰 만료기간 + 1초

--- backend/src/main/java/com/bungeobbang/backend/auth/domain/repository/UuidRepository.java
+++ backend/src/main/java/com/bungeobbang/backend/auth/domain/repository/UuidRepository.java
@@ -3,6 +3,7 @@ package com.bungeobbang.backend.auth.domain.repository;
 import com.bungeobbang.backend.auth.domain.Authority;
 import io.lettuce.core.api.sync.RedisCommands;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,9 +13,10 @@ import java.util.Optional;
 public class UuidRepository {
     private static final String AUTH_UUID = "uuid";
     private final RedisCommands<String, String> redisCommands;
+    @Value("${security.jwt.access-expiration-time}") Long accessExpirationTime;
 
     public void save(Authority role, String uuid, String id) {
-        redisCommands.set(String.format("%s_%s:%s", role, AUTH_UUID, id), uuid);
+        redisCommands.setex(String.format("%s_%s:%s", role, AUTH_UUID, id), accessExpirationTime + 1000, uuid);
     }
 
     public Optional<String> get(Authority role, String id) {
