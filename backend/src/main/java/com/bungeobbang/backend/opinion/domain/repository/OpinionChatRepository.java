e6514753874b5f842bde029f64203e895f7039ac
scc9811 <wjsrudtjr98@naver.com>
2025-02-14 20:59:25 +0900

    chore : 누락된 수정사항 추가

--- backend/src/main/java/com/bungeobbang/backend/opinion/domain/repository/OpinionChatRepository.java
+++ backend/src/main/java/com/bungeobbang/backend/opinion/domain/repository/OpinionChatRepository.java
@@ -2,8 +2,10 @@ package com.bungeobbang.backend.opinion.domain.repository;
 
 import com.bungeobbang.backend.opinion.domain.OpinionChat;
 import org.bson.types.ObjectId;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -34,6 +36,9 @@ public interface OpinionChatRepository extends MongoRepository<OpinionChat, Stri
             "{ $replaceRoot: { newRoot: '$chat' } }"
     })
     List<OpinionChat> findLatestChatsByOpinionIds(List<Long> opinionIds);
+
+    @Query("{ 'opinionId': ?0, '_id': { ?1: ?2 } }")
+    List<OpinionChat> findOpinionChats(Long opinionId, String comparison, ObjectId chatId, Pageable pageable);
 
     // opinionId에 해당하는 최신 채팅 1개를 가져오는 메서드
     Optional<OpinionChat> findTop1ByOpinionIdOrderByIdDesc(Long opinionId);
