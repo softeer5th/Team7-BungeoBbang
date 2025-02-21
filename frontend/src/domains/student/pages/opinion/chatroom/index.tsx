ee84d38ac6dc7917b372cf2ebe7cb8d15e58ba15
uuranus <hyur0920@gmail.com>
2025-02-21 20:32:00 +0900

    fix: #392 학생 말해요 채팅 하나일 때 렌더링 막히는 문제 해결

--- frontend/src/domains/student/pages/opinion/chatroom/index.tsx
+++ frontend/src/domains/student/pages/opinion/chatroom/index.tsx
@@ -91,6 +91,7 @@ const OpinionChatPage = () => {
             getReloadChatDataFromRecent();
           }
         } else {
+          console.log('gethasdownmor', getHasDownMore(), isWatchingBottom());
           if (!getHasDownMore()) {
             if (isWatchingBottom()) {
               isLiveReceiveChatAdded.current = true;
@@ -344,12 +345,18 @@ const OpinionChatPage = () => {
     setChatData((prev) => [...prev, newChat]);
   };
 
-  const { setTriggerUpItem, setTriggerDownItem, getHasDownMore, setHasUpMore, setHasDownMore } =
-    useInfiniteScroll({
-      initialFetch: getInitialChatData,
-      fetchUpMore: getMoreUpChatData,
-      fetchDownMore: getMoreDownChatData,
-    });
+  const {
+    setTriggerUpItem,
+    setTriggerDownItem,
+    getHasUpMore,
+    getHasDownMore,
+    setHasUpMore,
+    setHasDownMore,
+  } = useInfiniteScroll({
+    initialFetch: getInitialChatData,
+    fetchUpMore: getMoreUpChatData,
+    fetchDownMore: getMoreDownChatData,
+  });
 
   useLayoutEffect(() => {
     if (!elementRef.current) return;
@@ -467,7 +474,7 @@ const OpinionChatPage = () => {
       />
       <S.ChatList ref={elementRef}>
         {chatData.map((chat, chatIndex) => {
-          const isUpTriggerItem = chatIndex === FIRST_REMAIN_ITEMS;
+          const isUpTriggerItem = chatIndex === FIRST_REMAIN_ITEMS && getHasUpMore();
           const isDownTriggerItem = chatIndex === chatData.length - LAST_REMAIN_ITEMS;
 
           if (chat.type === ChatType.RECEIVE) {
