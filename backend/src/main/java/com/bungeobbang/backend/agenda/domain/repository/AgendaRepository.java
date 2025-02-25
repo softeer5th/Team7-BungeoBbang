4f0160b4638337f14f65fa291153a378eb418d19
zoouniak <jueun1025@naver.com>
2025-02-25 12:26:07 +0900

    fix: 통계 university 추가

--- backend/src/main/java/com/bungeobbang/backend/agenda/domain/repository/AgendaRepository.java
+++ backend/src/main/java/com/bungeobbang/backend/agenda/domain/repository/AgendaRepository.java
@@ -2,6 +2,7 @@ package com.bungeobbang.backend.agenda.domain.repository;
 
 import com.bungeobbang.backend.agenda.domain.Agenda;
 import com.bungeobbang.backend.agenda.dto.response.admin.AgendaCategoryResponse;
+import com.bungeobbang.backend.university.domain.University;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
@@ -39,28 +40,54 @@ public interface AgendaRepository extends JpaRepository<Agenda, Long> {
         // 특정 memberId에 대한 필터링
     List<Agenda> findActiveAgendasByMemberId(@Param("memberId") Long memberId);
 
-    @Query("SELECT COUNT(a) FROM Agenda a WHERE YEAR(a.createdAt) = :year AND MONTH(a.createdAt) = :month")
-    int countByCreatedAtBetween(@Param("year") int year, @Param("month") int month);
+    @Query("""
+            SELECT COUNT(a)
+                        FROM Agenda a
+                                    WHERE YEAR(a.createdAt) = :year
+                                                AND MONTH(a.createdAt) = :month
+                                                            AND a.university = :university
+            """)
+    int countByCreatedAtBetweenAndUniversity(@Param("year") int year, @Param("month") int month, @Param("universityId") University university);
 
-    @Query("SELECT coalesce( SUM(a.count),0) FROM Agenda a WHERE YEAR(a.createdAt) = :year AND MONTH(a.createdAt) = :month")
-    int countAgendaMembersByMonth(@Param("year") int year, @Param("month") int month);
+    @Query("""
+            SELECT coalesce( SUM(a.count),0)
+                        FROM Agenda a
+                                    WHERE YEAR(a.createdAt) = :year
+                                                AND MONTH(a.createdAt) = :month
+                                                            AND a.university = :university
+            """)
+    int countAgendaMembersByMonthAndUniversity(@Param("year") int year, @Param("month") int month, @Param("university") University university);
 
-    @Query("SELECT new com.bungeobbang.backend.agenda.dto.response.admin.AgendaCategoryResponse(a.categoryType, COUNT(a)) " +
-            "FROM Agenda a " +
-            "WHERE YEAR(a.createdAt) = :year AND MONTH(a.createdAt) = :month " +
-            "GROUP BY a.categoryType")
-    List<AgendaCategoryResponse> findCategoryStatisticsByMonth(@Param("year") int year, @Param("month") int month);
+    @Query("""
+            SELECT new com.bungeobbang.backend.agenda.dto.response.admin.AgendaCategoryResponse(a.categoryType, COUNT(a))
+                        FROM Agenda a
+                        WHERE a.university = :university And YEAR(a.createdAt) = :year AND MONTH(a.createdAt) = :month
+                        GROUP BY a.categoryType
+            """)
+    List<AgendaCategoryResponse> findCategoryStatisticsByMonthAndUniversity(@Param("year") int year, @Param("month") int month, @Param("university") University university);
 
 
-    @Query("SELECT COUNT(a) FROM Agenda a WHERE YEAR(a.createdAt) = :year")
-    int countAgendaByYear(@Param("year") int year);
+    @Query("""
+             SELECT COUNT(a) FROM Agenda a
+                         WHERE YEAR(a.createdAt) = :year
+                                     AND a.university = :university
+            """)
+    int countAgendaByYearAndUniversity(@Param("year") int year, @Param("university") University university);
 
-    @Query("SELECT coalesce( SUM(a.count),0) FROM Agenda a WHERE YEAR(a.createdAt) = :year")
-    int countAgendaMemberByYear(@Param("year") int year);
+    @Query("""
+            SELECT coalesce( SUM(a.count),0)
+                        FROM Agenda a
+            WHERE a.university = :university
+                        AND YEAR(a.createdAt) = :year
+            """)
+    int countAgendaMemberByYearAndUniversity(@Param("year") int year, @Param("university") University university);
 
-    @Query("SELECT new com.bungeobbang.backend.agenda.dto.response.admin.AgendaCategoryResponse(a.categoryType, COUNT(a)) " +
-            "FROM Agenda a " +
-            "WHERE YEAR(a.createdAt) = :year " +
-            "GROUP BY a.categoryType")
-    List<AgendaCategoryResponse> findCategoryStatisticsByYear(@Param("year") int year);
+    @Query("""
+            SELECT new com.bungeobbang.backend.agenda.dto.response.admin.AgendaCategoryResponse(a.categoryType, COUNT(a))
+                        FROM Agenda a
+                        WHERE  a.university = :university
+                                    AND YEAR(a.createdAt) = :year
+                        GROUP BY a.categoryType
+            """)
+    List<AgendaCategoryResponse> findCategoryStatisticsByYearAndUniversity(@Param("year") int year, @Param("university") University university);
 }
